076f13eddf1a5cb56f94a3ce77639fdc71fb5c8b
Ranajit Das <dasranajit410@gmail.com>
2023-03-29 19:46:35 +0530

    Initial commit

---
+++ frontend/src/Component/Calculator.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+import "../Style/Calculator.css"
+
+const Calculator = () => {
+  return (
+    <div>
+        <Navbar/>
+        hello
+    </div>
+  )
+}
+
+export default Calculator
---
+++ frontend/src/Component/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../Style/Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
