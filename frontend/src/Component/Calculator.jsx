04d07cbb92841323609dc90df7645111dd2f66d0
Ranajit Das <dasranajit410@gmail.com>
2023-03-29 21:46:53 +0530

    Clear function added

--- frontend/src/Component/Calculator.jsx
+++ frontend/src/Component/Calculator.jsx
@@ -10,13 +10,44 @@ import { BsBackspace } from "react-icons/bs";
 
 
 const Calculator = () => {
-    const [result,setResult]=useState(0)
-    const [data,setData]=useState("hello")
-    const handleNumber=()=>{
-
+    const [result,setResult]=useState("0")
+    const [data,setData]=useState("")
+    const handleNumber=(eachNum)=>{
+        setData((prev)=>prev+eachNum)
     }
-    const handleSign=()=>{
-
+    const handleSign=(eachOpe)=>{
+        setData((prev)=>prev+eachOpe)
+    }
+    const handleEqual=()=>{
+      const inputData=data
+      try{
+        const result=eval(inputData)
+        setResult(result.toString())
+      }
+      catch(err){
+        setResult("Error")
+      }
+    }
+    const handleClear=()=>{
+        setData("")
+        setResult("0")
+    }
+    const handleClearEnd=()=>{
+        let str=data.trim()
+        let delEle=""
+        for(let i=str.length-1;i>=0;i--){
+            if(str[i]=="+"||str[i]=="-"||str[i]=="*"||str[i]=="/"){
+                delEle+=str[i]
+                break
+            }
+        }
+        let newData=data.replace(delEle,"")
+        setData(newData)
+    }
+    const handleBackspace=()=>{
+        let newData=data.split("")
+        newData.pop()
+        setData(newData.join(""))
     }
   return (
     <div>
@@ -25,26 +56,26 @@ const Calculator = () => {
           <div className='result'>{result}</div>
           <div className='data'>{data}</div>
           <div className="allbtn">
-            <button onClick={()=>handleNumber()}>CE</button>
-            <button onClick={()=>handleNumber()}>C</button>
-            <button onClick={()=>handleNumber()}><BsBackspace/></button>
-            <button onClick={()=>handleNumber()}><RiDivideFill/></button>
-            <button onClick={()=>handleNumber()}>7</button>
-            <button onClick={()=>handleNumber()}>8</button>
-            <button onClick={()=>handleNumber()}>9</button>
-            <button onClick={()=>handleNumber()}><RxCross2/></button>
-            <button onClick={()=>handleNumber()}>4</button>
-            <button onClick={()=>handleNumber()}>5</button>
-            <button onClick={()=>handleNumber()}>6</button>
-            <button onClick={()=>handleNumber()}><AiOutlineMinus/></button>
-            <button onClick={()=>handleNumber()}>1</button>
-            <button onClick={()=>handleNumber()}>2</button>
-            <button onClick={()=>handleNumber()}>3</button>
-            <button onClick={()=>handleNumber()}>+</button>
+            <button onClick={()=>handleClearEnd()}>CE</button>
+            <button onClick={()=>handleClear()}>C</button>
+            <button onClick={()=>handleBackspace()}><BsBackspace/></button>
+            <button onClick={()=>handleSign("/")}><RiDivideFill/></button>
+            <button onClick={()=>handleNumber("7")}>7</button>
+            <button onClick={()=>handleNumber("8")}>8</button>
+            <button onClick={()=>handleNumber("9")}>9</button>
+            <button onClick={()=>handleSign("*")}><RxCross2/></button>
+            <button onClick={()=>handleNumber("4")}>4</button>
+            <button onClick={()=>handleNumber("5")}>5</button>
+            <button onClick={()=>handleNumber("6")}>6</button>
+            <button onClick={()=>handleSign("-")}><AiOutlineMinus/></button>
+            <button onClick={()=>handleNumber("1")}>1</button>
+            <button onClick={()=>handleNumber("2")}>2</button>
+            <button onClick={()=>handleNumber("3")}>3</button>
+            <button onClick={()=>handleSign("+")}>+</button>
             <button></button>
-            <button onClick={()=>handleNumber()}>0</button>
-            <button onClick={()=>handleNumber()}>.</button>
-            <button onClick={()=>handleNumber()}>=</button>
+            <button onClick={()=>handleNumber("0")}>0</button>
+            <button onClick={()=>handleSign(".")}>.</button>
+            <button onClick={()=>handleEqual()}>=</button>
           </div>
         </div>
     </div>
